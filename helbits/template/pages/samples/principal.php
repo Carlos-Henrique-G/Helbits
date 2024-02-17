8a489b9b5781a04f0de1c6a6cffccc3d34a75360
JonasCandido <jonasdcandido210@gmail.com>
2024-02-17 17:29:29 -0300

    update

--- helbits/template/pages/samples/principal.php
+++ helbits/template/pages/samples/principal.php
@@ -78,7 +78,7 @@
             </div>
           </li>
           <li class="nav-item nav-category">
-            <span class="nav-link">Navigation</span>
+            <span class="nav-link">Navegação</span>
           </li>
           <li class="nav-item menu-items">
             <a class="nav-link" href="../../index.html">
@@ -331,9 +331,9 @@
               <div class="col-md-12 col-md-4 grid-margin stretch-card">
                 <div class="card">
                   <div class="card-body">
-                    <h4 class="card-title">To do list</h4>
+                    <h4 class="card-title">Atividades</h4>
                     <div class="add-items d-flex">
-                      <input type="text" class="form-control todo-list-input" placeholder="enter task..">
+                      <input type="text" class="form-control todo-list-input" placeholder="Adicione tarefas">
                       <button class="add btn btn-primary todo-list-add-btn">Add</button>
                     </div>
                     <div class="list-wrapper">
@@ -341,38 +341,11 @@
                         <li>
                           <div class="form-check form-check-primary">
                             <label class="form-check-label">
-                              <input class="checkbox" type="checkbox"> Create invoice </label>
+                              <input class="checkbox" type="checkbox"> Atacar a Argentina </label>
                           </div>
                           <i class="remove mdi mdi-close-box"></i>
                         </li>
-                        <li>
-                          <div class="form-check form-check-primary">
-                            <label class="form-check-label">
-                              <input class="checkbox" type="checkbox"> Meeting with Alita </label>
-                          </div>
-                          <i class="remove mdi mdi-close-box"></i>
-                        </li>
-                        <li class="completed">
-                          <div class="form-check form-check-primary">
-                            <label class="form-check-label">
-                              <input class="checkbox" type="checkbox" checked> Prepare for presentation </label>
-                          </div>
-                          <i class="remove mdi mdi-close-box"></i>
-                        </li>
-                        <li>
-                          <div class="form-check form-check-primary">
-                            <label class="form-check-label">
-                              <input class="checkbox" type="checkbox"> Plan weekend outing </label>
-                          </div>
-                          <i class="remove mdi mdi-close-box"></i>
-                        </li>
-                        <li>
-                          <div class="form-check form-check-primary">
-                            <label class="form-check-label">
-                              <input class="checkbox" type="checkbox"> Pick up kids from school </label>
-                          </div>
-                          <i class="remove mdi mdi-close-box"></i>
-                        </li>
+                       
                       </ul>
                     </div>
                   </div>
