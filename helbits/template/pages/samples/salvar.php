d3302275173b70d313d61e21828bf3915f24d132
JonasCandido <jonasdcandido210@gmail.com>
2024-02-18 18:44:16 -0300

    finished login/register crud

--- helbits/template/pages/samples/salvar.php
+++ helbits/template/pages/samples/salvar.php
@@ -5,12 +5,18 @@
     $email = $_POST['email'];
     $senha = $_POST['senha'];
 
-    $sql = "insert into tbusuarios (cod_usu,nome,senha,email)
+    $novousuario = "insert into tbusuarios (cod_usu,nome,senha,email)
                                     values (null,'$nome','$senha','$email')";
 
-    $insert = $conexao->query($sql);
+    $insert = $conexao->query($novousuario);
 
-    if($insert==true) {
+    $usuarioconectado = "select * from tbusuarios where email='$email' and senha='$senha'";
+
+    $consulta = $conexao->query($usuarioconectado)
+
+    if($insert==true && $consulta-> num_rows > 0) {
+        session_start();
+        $_SESSION['login'] == 'ok';
         header('Location: principal.php?insert=ok');
     } else {
         header('Location: register.php?insert=erro');
