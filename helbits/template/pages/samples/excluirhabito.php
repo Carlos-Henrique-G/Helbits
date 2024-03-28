79a8c00c8e187be546e181a4235128bebf3c5eb8
Jonas Dias Cândido <jonasdcandido210@gmail.com>
2024-03-28 19:55:25 -0300

    updated delete habit

--- helbits/template/pages/samples/excluirhabito.php
+++ helbits/template/pages/samples/excluirhabito.php
@@ -1,6 +1,20 @@
 <?php
     include('./conexaocombanco/banco.php');
 
-    
+    session_start();
 
+    $habitoid = $_GET['id'];
+
+    for($i = 0;$i < count($_SESSION['habitosids']); $i++) {
+        if($_SESSION['habitos'][$i] == $habitoid) {
+            $sql_deletar_habitos = "delete from tbhabitos
+                              where cod_hab = $habitoid;";
+            $excluirhabitos = $conexao->query($sql_deletar_habitos);
+            if($excluirhabitos == true) {
+                header('Location: principal.php?habitodeletado;');
+            } else {
+                header('Location: principal.php?erroaodeletarhabito;');
+            }
+        }
+    }
 ?>
