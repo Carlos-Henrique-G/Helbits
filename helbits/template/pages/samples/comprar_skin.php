07ca2b01efcb785a12b97e9fc4e855b752cc4ae2
Carlos-Henrique-G <158851796+Carlos-Henrique-G@users.noreply.github.com>
2024-06-23 15:22:12 -0300

    .
    .

--- helbits/template/pages/samples/comprar_skin.php
+++ helbits/template/pages/samples/comprar_skin.php
@@ -3,10 +3,10 @@ include('./conexaocombanco/banco.php');
 session_start();
 $compra = $_POST['skin'];
 $codusu = $_SESSION['cod_usu'];
+
 $sql = "insert into inventario_skins (cod_inventario,cod_usu,cod_skin)
                                     values (null,'$codusu','$compra')";
-
-    
+   
 
     $insert = $conexao->query($sql);
     if($insert == true){
